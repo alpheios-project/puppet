5a6229886793d9564501b7ffdeae9f7ac7be2d61
Bridget Almas <balmas@gmail.com>
2017-10-10 13:37:50 +0000

    redirects for alpheios.net

--- site-modules/profile/manifests/www.pp
+++ site-modules/profile/manifests/www.pp
@@ -11,8 +11,17 @@ class profile::www {
 
    apache::vhost { 'www.melampus.org':
      port          => '80',
-     serveraliases => [ 'melampus.org'],
+     serveraliases => [ 'melampus.org','dev.alpheios.net'],
      docroot       => '/var/www/landing-page/build',
+     proxy_pass    =>   [ 
+       { 'path'    => '/content', 'url' => 'http://archive.alpheios.net/content'},
+       { 'path'    => '/sites', 'url' => 'http://archive.alpheios.net/sites'},
+       { 'path'    => '/alpheios-texts', 'url' => 'http://archive.alpheios.net/alpheios-texts' },
+       { 'path'    => '/alpheios-demos', 'url' => 'http://archive.alpheios.net/alpheios-demos' },
+       { 'path'    =>  '/poetry', 'url' => 'http://archive.alpheios.net/poetry'},
+       { 'path'    =>  '/perl', 'url' => 'http://archive.alpheios.net/perl' },
+       { 'path'    =>  '/xpi-install', 'url' => 'http://archive.alpheios.net/xpi-install' },
+       { 'path'    =>  '/xpi-updates', 'url' => 'http://archive.alpheios.net/xpi-updates' },
+     ],
    }
-                                                                                     
 }
