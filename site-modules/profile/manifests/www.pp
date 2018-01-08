9ac8452177e7c277eabbfd2af359771e30b24431
Bridget Almas <balmas@gmail.com>
2018-01-08 16:27:30 +0000

    add ssl

--- site-modules/profile/manifests/www.pp
+++ site-modules/profile/manifests/www.pp
@@ -46,4 +46,32 @@ class profile::www {
        }
      ]
    }
+
+   apache::vhost { 'ssl-alpheios':
+     port          => '443',
+     serveraliases => [ 'alpheios.net'],
+     docroot       => '/var/www/landing-page/build',
+     proxy_pass    =>   [ 
+       { 'path'    => '/content', 'url' => 'http://archive.alpheios.net/content'},
+       { 'path'    => '/sites', 'url' => 'http://archive.alpheios.net/sites'},
+       { 'path'    =>  '/poetry', 'url' => 'http://archive.alpheios.net/poetry'},
+       { 'path'    =>  '/perl/latin', 'url' => 'http://morph.alpheios.net/legacy/latin' },
+       { 'path'    =>  '/perl/greek', 'url' => 'http://morph.alpheios.net/legacy/greek' },
+       { 'path'    =>  '/perl/aramorph2', 'url' => 'http://morph.alpheios.net/legacy/aramorph2' },
+       { 'path'    =>  '/xpi-install', 'url' => 'http://archive.alpheios.net/xpi-install' },
+       { 'path'    =>  '/xpi-updates', 'url' => 'http://archive.alpheios.net/xpi-updates' },
+     ],
+     aliases   => [
+       { alias => '/alpheios-demos',
+         path  => '/var/www/demos',
+       },
+       { alias => '/alpheios-texts',
+         path  => '/var/www/enhanced-texts-v1',
+       }
+     ],
+     ssl        => true,
+     ssl_cert   => '/etc/ssl/certs/STAR_alpheios.net.crt',
+     ssl_key    => '/etc/ssl/private/Alpheios.key',
+     ssl_chain  => '/etc/ssl/certs/ca-bundle-client.crt',
+   }
 }
