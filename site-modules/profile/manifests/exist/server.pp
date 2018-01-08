120036a97c4bf0224102346f4a9163120c1737de
root <Bridget Almas>
2018-01-08 16:36:15 +0000

    add ssl

--- site-modules/profile/manifests/exist/server.pp
+++ site-modules/profile/manifests/exist/server.pp
@@ -27,5 +27,24 @@ class profile::exist::server {
     headers    => $headers,
 
   }
+  
+  apache::vhost { 'repos1-ssl':
+    port              => '443',
+    servername        => 'repos1.alpheios.net',
+    docroot           => '/var/www/html',
+    proxy_pass        => [ $proxy_pass ],
+    rewrites          => [ 
+      {'rewrite_rule' => [ '/exist/rest/db/xq/alpheios-text.xq http://repos-archive.alpheios.net/exist/rest/db/xq/alpheios-text.xq']},
+      {'rewrite_rule' => [ '/exist/rest/db/xq/alpheios-get-ref.xq http://repos-archive.alpheios.net/exist/rest/db/xq/alpheios-get-ref.xq']},
+      {'rewrite_rule' => [ '/exist/rest/db/xq/alpheios-get-toc.xq http://repos-archive.alpheios.net/exist/rest/db/xq/alpheios-get-toc.xq']},
+      {'rewrite_rule' => [ '/exist/rest/db/app/(.*)$ http://tools.alpheios.net/exist/rest/db/app/$1']},
+    ],
+    headers    => $headers,
+    ssl        => true,
+    ssl_cert   => '/etc/ssl/certs/STAR_alpheios.net.crt',
+    ssl_key    => '/etc/ssl/private/Alpheios.key',
+    ssl_chain  => '/etc/ssl/certs/ca-bundle-client.crt',
+
+  }
 
 }
