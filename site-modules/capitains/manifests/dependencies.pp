0001348257c722522fd1e72d34908953db229c8d
Bridget Almas <balmas@gmail.com>
2018-01-12 15:42:44 +0000

    put some limits on the redis memory usage

--- site-modules/capitains/manifests/dependencies.pp
+++ site-modules/capitains/manifests/dependencies.pp
@@ -3,7 +3,9 @@ class capitains::dependencies {
 
 
   class { 'redis': 
-    stop_writes_on_bgsave_error => false
+    stop_writes_on_bgsave_error => false,
+    maxmemory        => '1gb',
+    maxmemory_policy => 'allkeys-lru',
   }
 
   ensure_packages(hiera('capitains::deps'))
