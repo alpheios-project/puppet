f64177f04b1ee1b15c5918f24212d259b824adba
Bridget Almas <balmsa@gmail.com>
2018-04-11 13:53:01 -0400

    another fix to pwa role

--- site-modules/role/manifests/pwa.pp
+++ site-modules/role/manifests/pwa.pp
@@ -1,6 +1,6 @@
 # Be an eXist repository
 class role::pwa {
   include profile::common
-  include profile::tools::pwa
-  include profile::tools::pwa
+  include profile::pwa::build
+  include profile::pwa::server
 }
