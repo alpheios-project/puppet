92ddf4d381b17a578a81f0c9174c223116afcc0f
Bridget Almas <balmas@gmail.com>
2018-10-18 12:50:45 +0000

    add firewall to tools

--- site-modules/role/manifests/tools.pp
+++ site-modules/role/manifests/tools.pp
@@ -3,4 +3,10 @@ class role::tools {
   include profile::common
   include profile::tools::build
   include profile::tools::server
+
+  firewall { '100 Web Service Access':
+    proto  => 'tcp',
+    dport  => ['80','443'],
+    action => 'accept',
+  }
 }
