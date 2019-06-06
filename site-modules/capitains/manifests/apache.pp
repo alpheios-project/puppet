6d8dcd1e02a7f86bb5f9d27fedfe7e3549947a4f
Bridget Almas <balmas@gmail.com>
2019-06-06 15:19:48 +0000

    forcing to https and using beta ga id

--- site-modules/capitains/manifests/apache.pp
+++ site-modules/capitains/manifests/apache.pp
@@ -1,5 +1,6 @@
 # Webserver setup for Capitains
 class capitains::apache {
+  $servername = hiera('capitains::domain')
 
   $proxy_pass_dts_base = {
     'path'    => '/api/dts',
@@ -27,25 +28,15 @@ class capitains::apache {
   ]
 
   apache::vhost { 'texts':
-    servername => hiera('capitains::domain'),
-    port       => '80',
-    docroot    => $capitains::www_root,
-    proxy_pass => [$proxy_pass_not, $proxy_pass_dts_base, $proxy_pass_dts, $proxy_pass],
-    headers    => $headers,
-    aliases    => [
-      {
-        alias    => '/assets/nemo.secondary/static',
-        path     => "${capitains::app_root}/alpheios_nemo_ui/data/assets/images",
-      },
-      {
-        alias    => '/assets/nemo.secondary',
-        path     => "${capitains::app_root}/alpheios_nemo_ui/data/assets",
-      },
-    ],
+    servername      => $servername,
+    port            => '80',
+    docroot         => $capitains::www_root,
+    redirect_status => 'permanent',
+    redirect_dest   => "https://${servername}"
   }
 
   apache::vhost { 'ssl-texts':
-    servername => hiera('capitains::domain'),
+    servername => $servername,
     port          => '443',
     docroot    => $capitains::www_root,
     proxy_pass => [$proxy_pass_not, $proxy_pass_dts_base, $proxy_pass_dts, $proxy_pass],
