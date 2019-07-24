b7fe72dd9be664daa523dbedac89d7501a9e4126
Bridget Almas <balmas@gmail.com>
2019-07-24 12:32:30 +0000

    update ssl cipher suite for capitains

--- site-modules/capitains/manifests/apache.pp
+++ site-modules/capitains/manifests/apache.pp
@@ -76,6 +76,7 @@ class capitains::apache {
     ssl_cert        => '/etc/ssl/certs/STAR_alpheios.net.crt',
     ssl_key         => '/etc/ssl/private/Alpheios.key',
     ssl_chain       => '/etc/ssl/certs/ca-bundle-client.crt',
+    ssl_cipher      => 'EECDH+AESGCM:EDH+AESGCM:AES256+EECDH:ECDHE-RSA-AES128-SHA:DHE-RSA-AES128-GCM-SHA256:AES256+EDH:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES128-SHA256:ECDHE-RSA-AES256-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES128-SHA256:DHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA:ECDHE-RSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:AES256-GCM-SHA384:AES128-GCM-SHA256:AES256-SHA256:AES128-SHA256:AES256-SHA:AES128-SHA:DES-CBC3-SHA:HIGH:!aNULL:!eNULL:!EXPORT:!DES:!MD5:!PSK:!RC4',
     custom_fragment => 'DeflateBufferSize 16192', # needed to avoid transfer-encoding chunked for alpheios-embedded.js
   }
 
