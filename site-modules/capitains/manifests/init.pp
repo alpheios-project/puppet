44f7926d2dd5d0c422ac23ed6a981294992d344e
Bridget Almas <balmas@gmail.com>
2019-09-27 13:57:09 +0000

    update nemo release to fix dependencies

--- site-modules/capitains/manifests/init.pp
+++ site-modules/capitains/manifests/init.pp
@@ -21,7 +21,7 @@ class capitains($www_root,
 
   vcsrepo { $app_root:
      ensure   => latest,
-     revision => 'v3.0.0-rc.2',
+     revision => 'v3.0.0-rc.2-patch1',
      provider => git,
      source   => "https://github.com/alpheios-project/alpheios_nemo_ui",
      notify   => Python::Virtualenv[$app_root],
