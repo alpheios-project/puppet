44e040a8dea863b383e13ca39e6147a54d10fa54
Bridget Almas <balmas@gmail.com>
2019-12-18 20:03:34 +0000

    remove default apache host on tools

--- site-modules/profile/manifests/tools/server.pp
+++ site-modules/profile/manifests/tools/server.pp
@@ -1,8 +1,12 @@
 # Run the Handle Server container
 class profile::tools::server {
-  include apache
   include profile::docker::runner
   include tools::server
+
+  class {'apache':
+    log_formats   => { combined => '%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\" %D'},
+    default_vhost => false,
+  }
 
   $proxy_pass = {
     'path'    => '/exist/',
