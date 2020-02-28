c15a7696343f04371a75634a94dc1bc4fb5c48ef
Bridget Almas <balmas@gmail.com>
2020-02-28 20:49:38 +0000

    add mjp shortdefs

--- site-modules/profile/manifests/lexdata.pp
+++ site-modules/profile/manifests/lexdata.pp
@@ -67,4 +67,11 @@ class profile::lexdata {
      source   => 'https://github.com/alpheios-project/lan.git'
    }
 
+   vcsrepo { '/usr/local/lexdata/majorplus':
+     ensure   => latest,
+     revision => 'master',
+     provider => git,
+     source   => 'https://github.com/alpheios-project/majorplus.git'
+   }
+
 }
